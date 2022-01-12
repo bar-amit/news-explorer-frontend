e0a0b6413c2562c25ec29ebb0f32e045a5580231
bar-amit <bar.amit@outlook.com>
2022-01-12 20:07:04 +0200

    feat: popup css

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -1,25 +1,88 @@
+import { useState } from 'react';
 import Popup from "../Popup/Popup";
 
+import './PopupWithForm.css';
+
 function PopupWithForm({isOpen, close}) {
+  const [ isNewUser, setIsNewUser ] = useState(false);
+  function switchForm(){
+    setIsNewUser(!isNewUser);
+  }
+
   return (
     <Popup isOpen={isOpen} close={close}>
       <form className="user-form">
-        <h3 className="user-form__title">Sign in</h3>
-        <label for="email" className="user-form__label">
-          Email
-        </label>
-        <input type="email" name="email" placeholder="Enter Email"></input>
-        <span className="user-form__error"></span>
-        <label for="password" className="user-form__label">
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter Email"
-        ></input>
-        <span className="user-form__error"></span>
-        <button type="submit">Sign in</button>
+        {isNewUser ? (
+          <>
+            <h3 className="user-form__title">Sign up</h3>
+            <label className="user-form__label">
+              Email
+              <input
+                className="user-form__input"
+                type="email"
+                name="email"
+                placeholder="Enter email"
+              ></input>
+              <span className="user-form__error"></span>
+            </label>
+            <label className="user-form__label">
+              Password
+              <input
+                className="user-form__input"
+                type="password"
+                name="password"
+                placeholder="Enter password"
+              ></input>
+              <span className="user-form__error"></span>
+            </label>
+            <label className="user-form__label">
+              Username
+              <input
+                className="user-form__input"
+                type="text"
+                name="username"
+                placeholder="Enter your username"
+              ></input>
+              <span className="user-form__error"></span>
+            </label>
+            <button className="user-form__button" type="submit">
+              Sign up
+            </button>
+            <p className="user-form__text">
+              or <span className="user-form__link" onClick={switchForm}>Sign in</span>
+            </p>
+          </>
+        ) : (
+          <>
+            <h3 className="user-form__title">Sign in</h3>
+            <label className="user-form__label">
+              Email
+              <input
+                className="user-form__input"
+                type="email"
+                name="email"
+                placeholder="Enter email"
+              ></input>
+              <span className="user-form__error"></span>
+            </label>
+            <label className="user-form__label">
+              Password
+              <input
+                className="user-form__input"
+                type="password"
+                name="password"
+                placeholder="Enter password"
+              ></input>
+              <span className="user-form__error"></span>
+            </label>
+            <button className="user-form__button" type="submit">
+              Sign in
+            </button>
+            <p className="user-form__text">
+              or <span className="user-form__link" onClick={switchForm}>Sign up</span>
+            </p>
+          </>
+        )}
       </form>
     </Popup>
   );
