e93192a72c2790cbf402cf34abf622e785b6f28e
bar-amit <bar.amit@outlook.com>
2022-01-13 12:19:11 +0200

    feat: sticky header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,44 @@
+import { useState, useEffect } from 'react';
 import Navigation from "../Navigation/Navigation";
 import './Header.css';
 
-function Header({signUser}){
-    return (
-      <header className="header">
-        <h1 className="header__title">NewsExplorer</h1>
-        <Navigation signUser={signUser} />
-      </header>
-    );
+const hiddenHeaderClass = 'header_hidden';
+const headerThemeClass = "header_theme_bright";
+
+function Header({ signUser }) {
+  const [scrollPosition, setScrollPosition] = useState(window.scrollY);
+  const [headerVisibilityClass, setHeaderVisibilityClass] = useState("");
+
+  function onScroll() {
+    const currentPosition = window.scrollY;
+    setScrollPosition(currentPosition);
+    if(!this.prevScroll) this.prevScroll = currentPosition;
+    else if (currentPosition < this.prevScroll) {
+      setHeaderVisibilityClass("");
+    } else if (headerVisibilityClass === "") {
+      setHeaderVisibilityClass(hiddenHeaderClass);
+    }
+    this.prevScroll = currentPosition;
+  }
+
+  useEffect(() => {
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    <header
+      className={`header ${headerVisibilityClass} ${
+        scrollPosition > 60 && !headerVisibilityClass
+          ? headerThemeClass
+          : ""
+      }`}
+    >
+      <h1 className="header__title">NewsExplorer</h1>
+      <Navigation signUser={signUser} />
+    </header>
+  );
 }
 
 export default Header;
