9f25cd53f1ec57e7a66e0073a2aaf171c9c9afbc
bar-amit <bar.amit@outlook.com>
2022-01-21 12:41:44 +0200

    feat: Popup with message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,22 @@ import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
 import Footer from "../Footer/Footer";
 import PopupWithForm from "../PopupWithForm/PopupWithForm";
+import PopupMessage from "../PopupMessage/PopupMessage";
 import "./App.css";
 
 function App() {
+  /* Message Popup */
+  const [isSuccesful, setIsSuccesful] = useState(false);
+  const [isMessagePopupOpen, setIsMessagePopupOpen] = useState(false);
+  function closeMessagePopup() {
+    setIsMessagePopupOpen(false);
+  }
+  function openMessagePopup(status=false) {
+    setIsSuccesful(status);
+    setIsMessagePopupOpen(true);
+  }
+
+  /* User Popup */
   const [isUserPopupOpen, setIsUserPopupOpen] = useState(false);
   function closeUserPopup() {
     setIsUserPopupOpen(false);
@@ -15,9 +28,13 @@ function App() {
   function openUserPopup() {
     setIsUserPopupOpen(true);
   }
+
   useEffect(() => {
     const handleEsc = (e) => {
-      if (e.key === "Escape") closeUserPopup();
+      if (e.key === "Escape") {
+        closeMessagePopup();
+        closeUserPopup();
+      };
     };
     if (isUserPopupOpen) window.addEventListener("keydown", handleEsc);
     return () => window.removeEventListener("keydown", handleEsc);
@@ -32,7 +49,17 @@ function App() {
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
       <Footer />
-      <PopupWithForm isOpen={isUserPopupOpen} close={closeUserPopup} />
+      <PopupWithForm
+        isOpen={isUserPopupOpen}
+        close={closeUserPopup}
+        openMessage={openMessagePopup}
+      />
+      <PopupMessage
+        isOpen={isMessagePopupOpen}
+        close={closeMessagePopup}
+        isSuccesful={isSuccesful}
+        openLogin={openUserPopup}
+      />
     </div>
   );
 }
