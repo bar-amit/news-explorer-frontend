3bc0d1222404f357ce10ecb6a4b421aaf6264293
bar-amit <bar.amit@outlook.com>
2022-01-23 12:25:39 +0200

    feat(newscard) add image alt, remove truncate

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,21 +1,9 @@
 import "./NewsCard.css";
 
-// TODO: make truncate function.
-// function truncate(text, limit, ellipsis='...'){
-//   if(text.length > limit)
-//     return text.slice(0,limit-3);
-//   return text;
-// }
-
-// const MAX_TEXTLENGTH = {
-//   'w-1440': 93,
-//   'w-768':
-// }
-
 function NewsCard({ image, title, link, text, source, date, children }) {
   return (
     <li className="news-card">
-      <img className="news-card__image" src={image} />
+      <img className="news-card__image" src={image} alt={title} />
       <p className="news-card__date">{date}</p>
       <h4 className="news-card__title">{title}</h4>
       <p className="news-card__text">{text}</p>
